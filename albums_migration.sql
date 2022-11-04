3c523584384740f7da67999e8f823bbf0bee2551
Fernando Lopez <flopez00032@gmail.com>
2022-11-04 16:04:50 -0500

    new table finished

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,13 +1,15 @@
 
-
 USE codeup_test_db;
 
-CREATE TABLE user (
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
     user_id INT NOT NULL AUTO_INCREMENT,
     artist VARCHAR(35),
     name VARCHAR(40),
-    release_date INT,
+    release_date DATE,
     sales FLOAT,
-    genre VARCHAR(20)
+    genre VARCHAR(20),
+    PRIMARY KEY (user_id)
 );
 
