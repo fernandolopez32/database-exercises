148e435800c9046c8695e5054f707bfd02baf556
Fernando Lopez <flopez00032@gmail.com>
2022-11-07 11:46:32 -0600

    added stuf to table

---
+++ albums_seeder.sql
@@ -0,0 +1,22 @@
+USE codeup_test_db;
+
+
+INSERT INTO albums (artist , name, release_date, sales, genre) VALUES
+    ('Michael Jackson', 'Thriller', 1982,50.2,'Pop, post-disco, funk, rock'),
+    ('AC/DC', 'Back in Black', 1980, 30.1, 'Hard rock'),
+    ('Eagles', 'Their Greatest Hits (1971–1975)', 1976, 41.2,'Country rock, soft rock, folk rock '),
+    ('Eagles', 'Hotel California', 1976, 31.8, 'Soft rock'),
+    ('Shania Twain', 'Come On Over', 1997, 30.4, 'Country, pop'),
+    ('Fleetwood Mac', 'Rumours ', 1977, 29.3, 'Soft rock'),
+    ('Bee Gees / Various artists','Saturday Night Fever', 1977, 22.1, 'Disco'),
+    ('Led Zeppelin', 'Led Zeppelin IV',	1971, 30.4, 'Hard rock, heavy metal, folk rock'),
+    ('Michael Jackson','Bad', 1987, 21.1,'Pop, rhythm and blues, funk and rock'),
+    ('Meat Loaf', 'Bat Out of Hell',1977,22,'Hard rock, glam rock, progressive rock'),
+    ('Alanis Morissette', 'Jagged Little Pill',1995, 25.4,'Alternative rock'),
+    ('Various artists','Dirty Dancing', 1987,  24.1,'Pop, rock, R&B'),
+    ('Celine Dion', 'Falling into You',	1996, 21.1,'Pop, soft rock'),
+    ('Adele', '21',2011, 27.1, 'Pop, soul'),
+    ('The Beatles' ,'1' ,2000, 23.2, 'Rock'),
+    ('Metallica', 'Metallica', 1991, 22.7,	'Heavy metal'),
+    ('Celine Dion', 'Let\'s Talk About Love', 	1997, 19.5,	'Pop, soft rock'),
+    ('Bob Marley & The Wailers', 'Legend: The Best of Bob Marley & The Wailers',1984, 22.9,'Reggae');
--- basic_statements.sql
+++ basic_statements.sql
@@ -1,3 +1,7 @@
 USE codeup_test_db;
 
-INSERT INTO albums (artist , name, release_date, sales, genre) VALUES ('Tupac', '2Pacalypse Now', 1991, 0.923, 'HipHop');
+INSERT INTO albums (artist , name, release_date, sales, genre) VALUES ('Tupac', '2Pacalypse Now', 19910616, 0.923, 'HipHop');
+
+INSERT INTO albums (artist , name, release_date, sales, genre) VALUES ('The Strokes', 'First Inpression of Earth', 20051230, 0.88, 'Alternative/Indie');
+
+UPDATE albums SET sales = 88.0 WHERE sales = 0.88;
