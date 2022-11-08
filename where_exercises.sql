287f1dd5621dc7f2cac873a5f8930dfb5dd39f3a
Fernando Lopez <flopez00032@gmail.com>
2022-11-08 11:46:31 -0600

    code golf

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,18 +1,9 @@
 USE employees;
 
-SELECT* FROM employees
-WHERE ((first_name = 'Irena' OR first_name = 'Vidya')
-   OR first_name = 'Maya') AND gender = 'M' ORDER BY first_name ;
+SELECT* FROM employees WHERE ((first_name = 'Irena' OR first_name = 'Vidya') OR first_name = 'Maya') AND gender = 'M' ORDER BY first_name ;
 
-SELECT emp_no, first_name, last_name FROM employees
-WHERE last_name LIKE 'E%' OR last_name LIKE '%e';
+SELECT emp_no, first_name, last_name FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%e';
 
-SELECT emp_no, first_name, last_name FROM employees
-WHERE last_name LIKE 'E%' AND last_name LIKE '%e';
+SELECT emp_no, first_name, last_name FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%e';
 
-SELECT emp_no, first_name, last_name FROM employees
-WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
-
-
-
-
+SELECT emp_no, first_name, last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
