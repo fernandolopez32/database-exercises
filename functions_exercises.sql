af3e0625dc1a4e7d96e20225dd090c271b6b41ce
Fernando Lopez <flopez00032@gmail.com>
2022-11-09 09:33:23 -0600

    exercise done

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -19,6 +19,9 @@ select * from employees where first_name in ('Irena','Vidya', 'Maya')order by la
 
 SELECT CONCAT(first_name,' ',last_name) AS name  FROM employees WHERE first_name LIKE 'E%'ORDER BY emp_no DESC;
 
-SELECT * FROM employees WHERE birth_date LIKE '%-12-25';
+SELECT * FROM employees WHERE MONTH(birth_date) = 12 AND DAY(birth_date) = 25 ORDER BY hire_date;
 
-SELECT * FROM employees WHERE (hire_date LIKE '199%') ORDER BY birth_date;
+SELECT * FROM employees WHERE (hire_date LIKE '199%') AND (MONTH(birth_date) = 12 AND DAY(birth_date) = 25) ORDER BY hire_date;
+
+SELECT first_name, last_name, DATEDIFF(NOW(),hire_date) AS DaysWorkedForCompanny FROM employees WHERE (hire_date LIKE '199%') AND (MONTH(birth_date) = 12 AND DAY(birth_date) = 25) ORDER BY hire_date;
+
